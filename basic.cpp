39ead50c0c58b5cbaa44aff38da7a6f3397803c1
olikurt1 <olikurt1@outlook.com>
2025-10-29 10:44:39 +0000

    Demonstrating that a next print can be moved onto a new line in different ways and that \n\n created a blank line in between current and next print

--- basic.cpp
+++ basic.cpp
@@ -3,8 +3,10 @@ using namespace std; //names for objects and variables can be used from standard
 
 
 int main(){ //function ot execute anything in the braces
-    cout << "Hello World"; //C++ output of hell world
-    cout << "I am learning C++"; //will output straight after d not on new line
+    cout << "Hello World \n\n"; //C++ output of hell world
+    cout << "I am learning C++" << endl; //will output straight after d not on new line
+    cout << "Also on a new line";
+    cout << 3 * 5; //can directly output integer arithmetic and just integers
     return 0;//ends main function 
 }
 
@@ -12,6 +14,9 @@ int main(){ //function ot execute anything in the braces
 // std::cout
 
 // a statement is an instruction and each statement in c++ should end with ;
+
+//\n ensures any further prints are on a new line
+// \n\n will make a blank line the current statement line. can also use endl outside the print statement
 
 
 
