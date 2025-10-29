d849fc81d1d46ed95247688858edb6d28829669b
olikurt1 <olikurt1@outlook.com>
2025-10-29 10:36:24 +0000

    Adding comments and demonstrating that c++ will output two statements in line if not explicitly told not to

--- basic.cpp
+++ basic.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //header file library allowing input and output with c++
+using namespace std; //names for objects and variables can be used from standard library
 
 
-int main(){
-    cout << "Hello World";
-    return 0;
+int main(){ //function ot execute anything in the braces
+    cout << "Hello World"; //C++ output of hell world
+    cout << "I am learning C++"; //will output straight after d not on new line
+    return 0;//ends main function 
 }
+
+//using namespace std can be avoided by adding std:: for required objects
+// std::cout
+
+// a statement is an instruction and each statement in c++ should end with ;
+
+
+
