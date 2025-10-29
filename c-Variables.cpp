9b04bbbfd21a74328c3cb1b4304d45a3ee4125b3
olikurt1 <olikurt1@outlook.com>
2025-10-29 17:45:53 +0000

    Creating a file for variables and showing different ways of initialising and declaring variables

---
+++ c-Variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    //creating an integer variable and outputting it
+    int myNum = 15;
+    cout << myNum << endl;
+
+    //doing the same as above but showing that the variable can be declared without assignment first
+    int myNum2;
+    myNum = 15; //assigning a value to the declared variable
+    cout << myNum << endl;
+}
